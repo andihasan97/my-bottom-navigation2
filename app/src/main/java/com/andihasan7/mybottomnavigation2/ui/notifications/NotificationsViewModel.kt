cb1e527c07770947c76c491729586ec045060864
andihasan97 <andihasanelfalakiy@gmail.com>
2022-10-16 21:02:22 +0700

    first commit: default template bottom navigation ac

---
+++ app/src/main/java/com/andihasan7/mybottomnavigation2/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.andihasan7.mybottomnavigation2.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
